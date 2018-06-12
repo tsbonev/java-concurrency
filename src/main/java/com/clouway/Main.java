7eb8fdae4a95f2aefb258cdb92fe36167ad6b8d0
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-12 13:01:08 +0300

    Complete double thread task

--- src/main/java/com/clouway/Main.java
+++ src/main/java/com/clouway/Main.java
@@ -1,8 +1,10 @@
 package com.clouway;
 
-import java.util.Scanner;
+import java.util.Currency;
 
 public class Main {
+
+    static boolean isFinished = false;
 
     public static class Counter implements Runnable{
 
@@ -19,15 +21,24 @@ public class Main {
         public void run() {
 
             while (count < limit) {
+
+                if(isFinished){
+                    Thread.currentThread().interrupt();
+                }
+
                 try {
                     Thread.sleep(100);
                     count++;
                     System.out.println(name + " is at : " + count);
                 } catch (InterruptedException e) {
                     System.out.println(name + " was interrupted at " + count);
+                    return;
                 }
             }
-            System.out.println(name + " finished counting to " + limit);
+
+                isFinished = true;
+                System.out.println(name + " finished counting to " + limit);
+
         }
     }
 
@@ -35,14 +46,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Counter counter1 = new Counter(30, "First thread");
-        Counter counter2 = new Counter(15, "Second thread");
+        Counter counter1 = new Counter(3, "First thread");
+        Counter counter2 = new Counter(5, "Second thread");
 
         Thread thread1 = new Thread(counter1);
-        Thread thread2 = new Thread(counter2);
-
         thread1.start();
+        Thread thread2 = new Thread(counter2);
         thread2.start();
+        //Does this count as two thread classes?
     }
 
 }
