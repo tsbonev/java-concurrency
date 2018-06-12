fc5f901979bcf3f205085fb2b4fd5ef152264fb8
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-12 14:39:21 +0300

    Revert to default code state

--- src/main/java/com/clouway/Main.java
+++ src/main/java/com/clouway/Main.java
@@ -1,18 +1,17 @@
 package com.clouway;
 
-import java.util.Currency;
 
 public class Main {
 
     static boolean isFinished = false;
 
-    public static class Counter implements Runnable{
+    public static class Counter implements Runnable {
 
         private int count = 0;
         private int limit;
         private final String name;
 
-        public Counter(int limit, String name){
+        public Counter(int limit, String name) {
             this.name = name;
             this.limit = limit;
         }
@@ -22,7 +21,7 @@ public class Main {
 
             while (count < limit) {
 
-                if(isFinished){
+                if (isFinished) {
                     Thread.currentThread().interrupt();
                 }
 
@@ -30,30 +29,32 @@ public class Main {
                     Thread.sleep(100);
                     count++;
                     System.out.println(name + " is at : " + count);
+
+
                 } catch (InterruptedException e) {
                     System.out.println(name + " was interrupted at " + count);
                     return;
                 }
             }
 
-                isFinished = true;
-                System.out.println(name + " finished counting to " + limit);
+            isFinished = true;
+            System.out.println(name + " finished counting to " + limit);
 
         }
-    }
 
+    }
 
 
     public static void main(String[] args) {
 
-        Counter counter1 = new Counter(3, "First thread");
+        Counter counter1 = new Counter(6, "First thread");
         Counter counter2 = new Counter(5, "Second thread");
 
         Thread thread1 = new Thread(counter1);
-        thread1.start();
         Thread thread2 = new Thread(counter2);
+
+        thread1.start();
         thread2.start();
-        //Does this count as two thread classes?
     }
 
 }
