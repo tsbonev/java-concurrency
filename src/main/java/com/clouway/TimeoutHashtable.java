e7b7b15bc84d545ed55d7efdfeba03763e808a97
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-14 11:17:47 +0300

    Add working timeout hashtable

---
+++ src/main/java/com/clouway/TimeoutHashtable.java
@@ -0,0 +1,71 @@
+package com.clouway;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class TimeoutHashtable<K, T> {
+
+    private class TimeoutTimer implements Runnable {
+
+        private K key;
+
+        public TimeoutTimer(K key){
+            this.key = key;
+        }
+
+        @Override
+        public synchronized void run() {
+            try {
+                wait(timeoutTime);
+                hashtable.remove(key);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    private long timeoutTime;
+
+    private Map<K, Thread> timeout = new Hashtable<>();
+
+    private Map<K, T> hashtable = new Hashtable<>();
+
+    public TimeoutHashtable(long timeoutTime){
+        this.timeoutTime = timeoutTime;
+    }
+
+    public void put(K key, T value){
+        hashtable.put(key, value);
+        timeout.put(key, getTimeoutThread(key));
+    }
+
+    public T get (K key){
+
+        T value = hashtable.get(key);
+
+        if(value != null) refreshTimeout(key);
+
+        return value;
+    }
+
+    public T remove(K key){
+
+        timeout.get(key).interrupt();
+        timeout.remove(key);
+
+        return hashtable.remove(key);
+    }
+
+    private Thread getTimeoutThread(K key){
+        Thread thread = new Thread(new TimeoutTimer(key));
+        thread.start();
+        return thread;
+    }
+
+
+    private void refreshTimeout(K key){
+        timeout.get(key).interrupt();
+        timeout.put(key, getTimeoutThread(key));
+    }
+
+
+}
